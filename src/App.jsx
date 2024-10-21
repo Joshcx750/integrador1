3fea6a54f3813c99921a03ef505d110f32f2e8c0
Joshcx750 <ing_jgerardo@hotmail.com>
2024-10-21 13:54:45 -0600

    cambio proyecto en equipo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
-    <div className="app">
-      <h1>Convertidor de XML a PDF</h1>
-      <div className="button-container">
-        <button className="upload-button">Cargar archivo</button>
-        <button className="download-button">Descargar archivo</button>
+    <div className="container">
+      {/* Título */}
+      <header>ABCQUIM</header>
+
+      {/* Menú de Logeo */}
+      <div className="login-menu">
+        <input type="text" placeholder="Usuario" />
+        <input type="password" placeholder="Contraseña" />
+        <button>Ingresar</button>
+        <div className="create-user">Crear usuario</div>
+      </div>
+
+      {/* Sección de Búsqueda */}
+      <div className="search-section">
+        <input type="text" placeholder="Ingresa tu búsqueda..." />
+        <button>Buscar</button>
       </div>
     </div>
   );
